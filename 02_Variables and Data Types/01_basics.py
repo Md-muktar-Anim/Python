ca409f9509c218d5239cb6ef41853d8a5d4e5247
Md-muktar-Anim <mukatruzzamananim@gmail.com>
2025-06-28 18:35:44 +0600

    Python Fundamentals

---
+++ 02_Variables and Data Types/01_basics.py
@@ -0,0 +1,21 @@
+# In Python, variables are used to store data that
+# can be used and manipulated throughout a program.
+# A variable is created  the moment you assign a value to it using the assignment operator (=).
+
+age = 16 # integer
+name = "Anim" # string
+CGPA = 4.98 # float
+
+# Rules of defining a variable in Python 
+
+# Variable names must start with a letter (a-z, A-Z) or an underscore (_).
+# They can contain letters, numbers, and underscores.
+# Variable names are case-sensitive (age and Age are different).
+# Avoid using Python keywords (e.g., if, for, while) as varible names.
+
+# 16age = 17 # Invalid because variables can not start with a number
+age = 20 # Valid because variables can start with a letter.
+#A$$$ge = 70 #Invalid because variables can not contain special characters other than (_).
+__age = 80 # Valid because it follows all of the rules of python
+___Anim_45 = 90 # Valid because it follows all of the rules of python
+a_b_c_7_8 = "Anim" # Valid because it follows all of the rules of python
---
+++ 02_Variables and Data Types/02_data_types.py
@@ -0,0 +1,26 @@
+#Python supports several built-in data types:
+
+#      Integers (int)  : Whole numbers (e.g., 10, -5).
+#      Floats (float)  : Decimal numbers (e.g, 3.14, -0.001).
+#      Strings (str)   :  Text data enclosed in quotes (e.g., "Hello World", "Python").
+#      Booleans (bool) : Represents True or False.
+#      Lists           : Ordered, mutable collections (e.g., [1, 2, 3]).
+#      Tuples          : Ordered, immutable collections (e.g., (1, 2, 3)).
+#      Sets            : Unordered collections of unique elements (e.g., {1, 2, 3})
+#      Dictionaries    : Key-value pairs  (e.g., {"Anim", "Muktar", "Anik"}).
+
+age = 16
+print(age)
+print(type(age))
+
+CGPA = 4.98
+print(CGPA)
+print(type(CGPA))
+
+name = "Anim"
+print(name)
+print(type(name))
+
+is_completed = True # can also be False
+print(is_completed)
+print(type(is_completed))
